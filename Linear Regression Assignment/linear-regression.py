bc0dca4e90b0bd5c3176a25f5bf095451bd45002
sameerCoder <39183783+sameerCoder@users.noreply.github.com>
2022-01-24 05:57:45 +0530

    assignment1

---
+++ Linear Regression Assignment/linear-regression.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt  
+from sklearn.linear_model import LinearRegression  
+import numpy as np
+from sklearn.metrics import mean_squared_error   
+
+'''
+We can use Scikit learn to build a simple linear regression model
+you can use it use it like 
+model = LinearRegression() 
+see the documentation here: 
+http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html
+
+To get Mean Squared Error (MSE) you can use 
+mean_squared_error(Actual,Prediction)
+'''
+
+
+#### Your Code Goes Here #### 
+
+## Step 1: Load Data from CSV File ####
+
+
+## Step 2: Plot the Data ####
+
+
+## Step 3: Build a Linear Regression Model ####
+
+
+## Step 4: Plot the Regression line ####
+
+
+## Step 5: Make Predictions on Test Data ####
+
+
+## Step 6: Estimate Error #### 
